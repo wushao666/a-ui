0fd0d8d2411834d588da7427036093d510ba3826
shawnwwu <wslsdust@163.com>
2025-02-22 21:23:47 +0800

    second commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="bg-slate-800 rounded-xl p-8 md:flex md:p-0">
+    <img 
+      src="https://p3-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/56b00087d93a4a84adef69adc1dc2c14~tplv-k3u1fbpfcp-zoom-in-crop-mark:1512:0:0:0.awebp" 
+      alt=""
+      class="w-24 h-24 rounded-full mx-auto md:w-48 md:h-auto md:rounded-none">
+    <div class="pt-6 text-center space-y-4 md:p-8 md:text-left">
+      <p class="text-lg font-medium text-zinc-100">
+        这是使用 tailwindcss来实现的一个基础的卡片区域，在实现该卡片区域的过程中，我们从来没有离开过
+      </p>
+      <div class="font-medium">
+        <div class="text-sky-400">shawnwwu</div>
+        <div class="text-slate-500">中前台解决方案</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
